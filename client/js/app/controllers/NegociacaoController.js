ef5d64a63a2b1e3ed5f18ef88198619eb7899ab6
Vitoria Fortes <fortesvitoria@gmail.com>
2023-05-16 10:12:43 -0300

    ajusta data da negociação

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -13,10 +13,18 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
+
         // a data vem como string, então é preciso transforma-la em um objeto tipo date
+        //separa o array nos -, faz mapeamento e no indice 1 (mes) decrementa 1.
+        //o mes começa de 0, portanto 0 é janeiro, 1 é fevereiro
+        //para não usar o if, usamos item - indice % 2. 0 - 2 = 0, 2-2 = 0 (nao decrementa), 1 % 2 = 1, decrementa
+        let data = new Date(
+            ...this._inputData.value.split('-').map((item, indice) => item - indice % 2)
+        );
+        
         let negociacao = new Negociacao(
+            data,
             this._inputQuantidade.value,
-            this._inputData.value,
             this._inputValor.value,
         )
 
