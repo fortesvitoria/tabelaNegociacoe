f0ec9470e2e2e5c48dc120602533c8d518f16662
Vitoria Fortes <fortesvitoria@gmail.com>
2023-05-16 10:17:35 -0300

    comenta codigo sobre a data

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -18,10 +18,11 @@ class NegociacaoController {
         //separa o array nos -, faz mapeamento e no indice 1 (mes) decrementa 1.
         //o mes começa de 0, portanto 0 é janeiro, 1 é fevereiro
         //para não usar o if, usamos item - indice % 2. 0 - 2 = 0, 2-2 = 0 (nao decrementa), 1 % 2 = 1, decrementa
+        // o spread operator... o primeiro item, vai ser o primeiro parametro passado,. o segundo, será o segundo a ser passado, etc.
         let data = new Date(
             ...this._inputData.value.split('-').map((item, indice) => item - indice % 2)
         );
-        
+
         let negociacao = new Negociacao(
             data,
             this._inputQuantidade.value,
